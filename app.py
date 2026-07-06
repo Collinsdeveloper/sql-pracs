d43aef22b41efb8794d5282461dae4f3cf705d1f
Collinsdeveloper <kipngetichcollins19@gmail.com>
2026-07-06 15:09:19 +0300

    created database

---
+++ app.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect("tasks.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS tasks(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT,
+    completed INTEGER
+)
+""")
+
+conn.commit()
+conn.close()
+
+print("Database created!")
